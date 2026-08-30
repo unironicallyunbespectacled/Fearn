cf212946385e3c3d30b3cdc78f0af5e2fa8aa396
unironicallyunbespectacled <praise.vc.16@gmail.com>
2026-08-30 17:49:18 -0300

    feat(russian): hand-author Unit 21 (ru-u21-l1..ru-u21-l5) - Russian participle system (active present/past -ushch/-yashch/-vsh/-sh; passive present/past -em/-im/-nn/-enn/-t; short passive forms; punctuation & academic discourse synthesis)

--- data/curricula/russian.ledger.js
+++ data/curricula/russian.ledger.js
@@ -104,14 +104,14 @@
     "ru-u20-l2",
     "ru-u20-l3",
     "ru-u20-l4",
-    "ru-u20-l5"
-  ],
-  "stubOnly": [
+    "ru-u20-l5",
     "ru-u21-l1",
     "ru-u21-l2",
     "ru-u21-l3",
     "ru-u21-l4",
-    "ru-u21-l5",
+    "ru-u21-l5"
+  ],
+  "stubOnly": [
     "ru-u22-l1",
     "ru-u22-l2",
     "ru-u22-l3",
@@ -178,8 +178,8 @@
     "ru-u34-l4",
     "ru-u34-l5"
   ],
-  "lastCompletedLessonId": "ru-u20-l5",
-  "nextToAuthor": "ru-u21-l1"
+  "lastCompletedLessonId": "ru-u21-l5",
+  "nextToAuthor": "ru-u22-l1"
 };
   global.FEARN_LEDGERS = global.FEARN_LEDGERS || {};
   global.FEARN_LEDGERS['russian'] = LEDGER;
