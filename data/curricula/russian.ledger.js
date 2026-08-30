5d34a312637fc72ee07b587682e6b1f46c773fd7
unironicallyunbespectacled <praise.vc.16@gmail.com>
2026-08-30 17:52:27 -0300

    feat(russian): hand-author Unit 22 (ru-u22-l1..ru-u22-l5) - Russian verbal adverbs / deeprichastiya (imperfective -a/-ya/-s; perfective -v/-vshis/-shi/-dya; single-subject rule & Chekhov constraint; adverb/preposition transitions & punctuation; narrative & historical essay synthesis)

--- data/curricula/russian.ledger.js
+++ data/curricula/russian.ledger.js
@@ -109,14 +109,14 @@
     "ru-u21-l2",
     "ru-u21-l3",
     "ru-u21-l4",
-    "ru-u21-l5"
-  ],
-  "stubOnly": [
+    "ru-u21-l5",
     "ru-u22-l1",
     "ru-u22-l2",
     "ru-u22-l3",
     "ru-u22-l4",
-    "ru-u22-l5",
+    "ru-u22-l5"
+  ],
+  "stubOnly": [
     "ru-u23-l1",
     "ru-u23-l2",
     "ru-u23-l3",
@@ -178,8 +178,8 @@
     "ru-u34-l4",
     "ru-u34-l5"
   ],
-  "lastCompletedLessonId": "ru-u21-l5",
-  "nextToAuthor": "ru-u22-l1"
+  "lastCompletedLessonId": "ru-u22-l5",
+  "nextToAuthor": "ru-u23-l1"
 };
   global.FEARN_LEDGERS = global.FEARN_LEDGERS || {};
   global.FEARN_LEDGERS['russian'] = LEDGER;
